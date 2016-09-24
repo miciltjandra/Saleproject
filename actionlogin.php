dccf3a6f48baf48df8204a6e5ee98471cde1c4b7
Michael <miciltjandra@gmail.com>
2016-09-24 20:04:01 +0700

    Add select and compare password

--- actionlogin.php
+++ actionlogin.php
@@ -1,9 +1,9 @@
 <?php
-	$user = $_POST["user"];
-	$pass = $_POST["pass"];
+	$username = $_POST["user"];
+	$password = $_POST["pass"];
 
-	echo $user . "<br/>\n";
-	echo $pass . "<br/>\n";
+	echo $username . "<br/>\n";
+	echo $password . "<br/>\n";
 
 	$servername = "localhost";
 	$db_username = "wbd";
@@ -18,5 +18,26 @@
 	    die("Connection failed: " . $conn->connect_error);
 	} else {
 		echo "Connected successfully <br>\n";
+	}
+
+	$query = "SELECT password
+	FROM user
+	WHERE username = '" . $username . "'";
+
+	echo $query . "<br>\n"; 
+
+	$result = $conn->query($query);
+
+	if ($result->num_rows == 0) {
+		echo "Username not found </br>\n";
+	}
+	$resultpassword = $result->fetch_assoc();
+
+	echo $resultpassword["password"] . "<br/>\n";
+
+	if ($resultpassword["password"] === $password) {
+		echo "Login successful <br/>\n";
+	} else {
+		echo "Login error <br/>\n";
 	}
 ?>
