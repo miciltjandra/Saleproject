0b530732a6f5a9b08f6bc03a7fd96519fd74626d
SimpliCty <ndwongso_nikolas@yahoo.com>
2016-09-29 20:06:25 +0700

    -a

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div>
-	<img src="resources/tbc.png" style=" float:left; width:100px;  margin-left: 10%; margin-right: 10px">
-	<br><br>
-	<div style="font-family:impact; font-size:36px">TBC Online Trading</div>
+	<div class="middle title">SaleProject</div>
 </div>
-<br><hr>
+<br>
+<br>
+<br>
