fbbd8a4f618142e2f200d9b8c0bb2426ae8c2c44
joshuakosasih <joshua.kosasih@gmail.com>
2016-09-30 07:37:11 +0700

    Add username, logout, menu bar, and style draft

--- header.php
+++ header.php
@@ -1,6 +1,47 @@
-<div>
+<head>
+<style type="text/css">
+	#tab {
+		background-color: black;
+		list-style-type: none;
+		overflow: hidden;
+		border-style: solid;
+		border-width: 0px 2px 0px 0px;
+	}
+	#tab li a {
+		background-color: gray;
+		border-style: solid;
+		border-width: 2px 0px 2px 2px;
+		padding: 16px;
+		float: left;
+		display: block;
+	}
+	#tab li a:hover {
+		background-color: lightblue;
+	}
+</style>
+</head>
+
+<body>
+<div id="logo">
 	<img src="resources/tbc.png" style=" float:left; width:100px;  margin-left: 10%; margin-right: 10px">
 	<br><br>
-	<div style="font-family:impact; font-size:36px">TBC Online Trading</div>
+	<div style="font-family:impact; font-size:36px">Sale Project</div>
 </div>
-<br><hr>
+<br />
+<div>
+<h5 style="float: right">Hi username</h5>
+<br />
+<br />
+<a style="float: right">logout</a>
+</div>
+<br />
+<ul id="tab">
+<li><a>Catalog</a></li>
+<li><a>Your Products</a></li>
+<li><a>Add Product</a></li>
+<li><a>Sales</a></li>
+<li><a>Purchases</a></li>
+</ul>
+
+
+</body>
