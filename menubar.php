4184f7135943a6c55e3b17076011e52ad5a9ca7d
SimpliCty <ndwongso_nikolas@yahoo.com>
2016-09-30 10:41:34 +0700

    Linked nav bar

--- menubar.php
+++ menubar.php
@@ -4,11 +4,11 @@
 	<a style="text-align: right">logout</a>
 </div>
 <ul id="tab">
-<li><a>Catalog</a></li>
-<li><a>Your Products</a></li>
-<li><a>Add Product</a></li>
-<li><a>Sales</a></li>
-<li><a>Purchases</a></li>
+<li><a href="catalog.php">Catalog</a></li>
+<li><a href="yourproducts.php">Your Products</a></li>
+<li><a href="addproduct.php">Add Product</a></li>
+<li><a href="sales.php">Sales</a></li>
+<li><a href="purchases.php">Purchases</a></li>
 </ul>
 <br><br>
 <br><br>
