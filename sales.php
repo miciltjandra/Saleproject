458a9dba8fe1d5c6dc4b37b964b08663a36287e0
SimpliCty <ndwongso_nikolas@yahoo.com>
2016-09-30 10:37:57 +0700

    update dummy page

---
+++ sales.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Sales </title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<?php
+	    require 'header.php';
+	    require_once 'header.php';
+		require 'menubar.php';
+		require_once 'menubar.php';
+    ?>
+	<body class="middle">
+		<?php
+			session_start();
+			$user = $_SESSION["login"];
+			if ($user == "") {
+				echo "Unlogged";
+				header("Location: index.php");
+			}
+			else {
+				echo "user : " . $user;
+				$_SESSION["login"] = "";
+			}
+		?>
+	</body>
+</html>
