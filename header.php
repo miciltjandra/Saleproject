2d4076637fc3777253a865597a7812bfccb4b1df
SimpliCty <ndwongso_nikolas@yahoo.com>
2016-09-30 08:36:49 +0700

    update style

--- header.php
+++ header.php
@@ -1,33 +1,7 @@
-<head>
-<style type="text/css">
-	#tab {
-		background-color: black;
-		list-style-type: none;
-		overflow: hidden;
-		border-style: solid;
-		border-width: 0px 2px 0px 0px;
-	}
-	#tab li a {
-		background-color: gray;
-		border-style: solid;
-		border-width: 2px 0px 2px 2px;
-		padding: 16px;
-		float: left;
-		display: block;
-	}
-	#tab li a:hover {
-		background-color: lightblue;
-	}
-</style>
-</head>
-
-<body>
-<div id="logo">
-	<img src="resources/tbc.png" style=" float:left; width:100px;  margin-left: 10%; margin-right: 10px">
-	<br><br>
-	<div style="font-family:impact; font-size:36px">Sale Project</div>
-</div>
-<br />
+<div class="middle title">SaleProject</div>
+<br><br>
+<br><br>
+<br>
 <div>
 <h5 style="float: right">Hi username</h5>
 <br />
@@ -42,6 +16,3 @@
 <li><a>Sales</a></li>
 <li><a>Purchases</a></li>
 </ul>
-
-
-</body>
