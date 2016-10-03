baff567b1d1ed32895cbda569d2d3c826c437a89
Michael Tjandra <miciltjandra@gmail.com>
2016-10-04 05:14:08 +0700

    Add id_active to every menu

--- menubar.php
+++ menubar.php
@@ -1,12 +1,13 @@
+<?php $user = $_GET['id_active'];?>
 <div class="middle fix">
 	<div style="text-align: right">Hi username</div><br>
 	<div style="text-align: right">logout</div><br>
 </div>
 <ul id="tab">
-<li><a href="catalog.php" 		<?php if (strpos($_SERVER['PHP_SELF'], 'catalog.php')) echo 'class="active"'; ?> 		>Catalog</a></li>
-<li><a href="yourproducts.php" 	<?php if (strpos($_SERVER['PHP_SELF'], 'yourproducts.php')) echo 'class="active"'; ?> 	>Your Products</a></li>
-<li><a href="addproduct.php" 	<?php if (strpos($_SERVER['PHP_SELF'], 'addproduct.php')) echo 'class="active"'; ?> 	>Add Product</a></li>
-<li><a href="sales.php" 		<?php if (strpos($_SERVER['PHP_SELF'], 'sales.php')) echo 'class="active"'; ?>			>Sales</a></li>
-<li><a href="purchases.php" 	<?php if (strpos($_SERVER['PHP_SELF'], 'purchases.php')) echo 'class="active"'; ?>		>Purchases</a></li>
+<li><a href="catalog.php?id_active=<?php echo $user;?>" 		<?php if (strpos($_SERVER['PHP_SELF'], 'catalog.php')) echo 'class="active"'; ?> 		>Catalog</a></li>
+<li><a href="yourproducts.php?id_active=<?php echo $user;?>" 	<?php if (strpos($_SERVER['PHP_SELF'], 'yourproducts.php')) echo 'class="active"'; ?> 	>Your Products</a></li>
+<li><a href="addproduct.php?id_active=<?php echo $user;?>" 	<?php if (strpos($_SERVER['PHP_SELF'], 'addproduct.php')) echo 'class="active"'; ?> 	>Add Product</a></li>
+<li><a href="sales.php?id_active=<?php echo $user;?>" 		<?php if (strpos($_SERVER['PHP_SELF'], 'sales.php')) echo 'class="active"'; ?>			>Sales</a></li>
+<li><a href="purchases.php?id_active=<?php echo $user;?>" 	<?php if (strpos($_SERVER['PHP_SELF'], 'purchases.php')) echo 'class="active"'; ?>		>Purchases</a></li>
 </ul>
 <br><br>
