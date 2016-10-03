bd812a6b589ed1028a5d7556b5e16e526d84592b
Michael Tjandra <miciltjandra@gmail.com>
2016-10-04 05:11:40 +0700

    Add search feature

--- catalog.php
+++ catalog.php
@@ -12,40 +12,53 @@
 		require_once 'menubar.php';
     ?>
 	<body class="middle">
+		<?php
+			//check submit
+			if (isset($_POST['submit_search'])) {
+				$type = $_POST['searchcategory'];
+				$val = $_POST['search'];
+				$query = $type . ' like "%' . $val . '%"'; 
+			}
+			else {
+				$query = '';
+			}
+			echo $query . 'a';
+		?>
 		
 		<h1> What are you going to buy today? </h1>
 		<hr/>
-		<input type="text" name="search" id="search" value="search">
-		<input type="radio" name="searchcategory" value="product"> product
-		<input type="radio" name="searchcategory" value="store"> store
-		<input type="submit" value="go">
+		<form action="catalog.php?id_active=<?php echo $_GET['id_active'];?>" method="post">
+		<input type="text" name="search" id="search" placeholder="search">
+		<input type="radio" name="searchcategory" value="product_name" checked="checked"> product
+		<input type="radio" name="searchcategory" value="username"> store
+		<input type="submit" name="submit_search" value="go">
 		<br/>
 		<table name="productlist">
-					<?php
+			<?php
 
-						include 'actioncatalog.php';
-						$result = retrieveproduct('');
-						while ($product = $result->fetch_assoc()) {
-							echo "<tr>\n";
-							echo "<td>";
-							echo $product["username"] . "<br/>\n";
-							echo "added this on " . $product["added_date"] . "<br/>\n";
-							echo '<img class="product_icon" height="100" src="data:image/jpg;base64,'. base64_encode($product["image"]) .'"/> <br/>' . "\n";
-							echo $product["product_name"] . "<br/>\n";
-							echo $product["price"] . "<br/>\n";
-							echo $product["description"] . "<br/>\n";
-							echo '<span id="'.$product["product_id"].'_like">' . $product["likes"] . "</span> likes <br/>\n";
-							echo $product["purchases"] . " purchases<br/>\n";
-							echo '<input type="button" value="Like" onclick="increaseLike(' . $product["product_id"] . ')"> <br/>';
-							echo '<a href="#"> Buy </a> <br/><br/><hr/>';
-							echo "</td>\n";
-							echo "</tr>";
-							echo "<br/>\n";
-						}
-						//echo $result["description"];
-						//echo $result;
+				include 'actioncatalog.php';
+				$result = retrieveproduct($query);
+				while ($product = $result->fetch_assoc()) {
+					echo "<tr>\n";
+					echo "<td>";
+					echo $product["username"] . "<br/>\n";
+					echo "added this on " . $product["added_date"] . "<br/>\n";
+					echo '<img class="product_icon" height="100" src="data:image/jpg;base64,'. base64_encode($product["image"]) .'"/> <br/>' . "\n";
+					echo $product["product_name"] . "<br/>\n";
+					echo $product["price"] . "<br/>\n";
+					echo $product["description"] . "<br/>\n";
+					echo '<span id="'.$product["product_id"].'_like">' . $product["likes"] . "</span> likes <br/>\n";
+					echo $product["purchases"] . " purchases<br/>\n";
+					echo '<input type="button" value="Like" onclick="increaseLike(' . $product["product_id"] . ')"> <br/>';
+					echo '<a href="confirm_purchase.php?id_active=' . $_GET['id_active'] .'&product='.$product["product_id"].'"> Buy </a> <br/><br/><hr/>';
+					echo "</td>\n";
+					echo "</tr>";
+					echo "<br/>\n";
+				}
+				//echo $result["description"];
+				//echo $result;
 
-					?>
+			?>
 		</table>
 		
 	</body>
