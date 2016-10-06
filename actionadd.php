15881fdd6d290a9bc5833cf45838ec5cbbee0925
joshuakosasih <joshua.kosasih@gmail.com>
2016-10-06 23:51:58 +0700

    add capabilities to upload image

--- actionadd.php
+++ actionadd.php
@@ -3,56 +3,38 @@
 	$prd_name = $_POST["name"];
 	$description = $_POST["desc"];
 	$price = $_POST["price"];
-	$image = $_POST["image"];
-/*
-	echo 'woyy<br />';
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["imagefile"]["name"]);
-	$uploadOK = 1;
-	$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-	// Check if image file is a actual image or fake image
-	echo $target_file . '<br />';
-	echo $imageFileType . '<br />';
 	
-	if(isset($_POST["addbtn"])) {
-	    $check = getimagesize($_FILES["imagefile"]["tmp_name"]);
-	    if($check !== false) {
-	        echo "File is an image - " . $check["mime"] . ".";
-	        $uploadOk = 1;
-	    } else {
-	        echo "File is not an image.";
-	        $uploadOk = 0;
-	    }
-	}
-	
-	// Check if file already exists
-	if (file_exists($target_file)) {
-	    echo "Sorry, file already exists.";
-	    $uploadOk = 0;
-	}
-	// Check file size
+	$upload_image = $_FILES["imagefile"]["name"];
+
+	$folder="images/";
+
 	if ($_FILES["imagefile"]["size"] > 5000000) {
-	    echo "Sorry, your file is too large. Max file size 5MB";
-	    $uploadOk = 0;
+
+	    echo "Sorry, your file is too large. Maks file size 5 MB.";
+	    
 	}
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-	    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-	    $uploadOk = 0;
+	else {
+		$imageFileType = pathinfo($_FILES["imagefile"]["name"],PATHINFO_EXTENSION);
+
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "bmp") {
+		    echo "Sorry, only JPG, JPEG, PNG, GIF, BMP files are allowed.";
+		    
+		}
+		else {
+			$fullname = $folder . $_FILES["imagefile"]["name"];
+
+			if (file_exists($fullname)) {
+			    echo "Sorry, file name already exists. Please change the image file name.";
+			}
+			else {
+				move_uploaded_file($_FILES["imagefile"]["tmp_name"], "$folder".$_FILES["imagefile"]["name"]);
+			}
+		}
 	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-	    echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-	} else {
-	    if (move_uploaded_file($_FILES["imagefile"]["tmp_name"], $target_file)) {
-	        echo "The file ". basename( $_FILES["imagefile"]["name"]). " has been uploaded.";
-	    } else {
-	        echo "Sorry, there was an error uploading your file.";
-	    }
-	}
-*/
+
+//	$insert_path="INSERT INTO image_table VALUES('$folder','$upload_image')";
+
+	
 
 
 	$servername = "localhost";
@@ -78,13 +60,14 @@
 		echo "Connected successfully <br>\n";
 	}
 
+
 	$query = "INSERT INTO product(product_name, description, added_date, price, image, seller_id)
 	VALUES(" .
 	"\"" . $prd_name . "\"" . ", " .
 	"\"" . $description . "\"" . ", " .
 	"\"" . date("Y-m-d H-i") . "\"" . ", " .
 	"\"" . $price . "\"" . ", " .
-	"\"" . $image . "\"" . ", " .
+	"\"" . $fullname . "\"" . ", " .
 	"\"" . $user_id . "\"" . ")";
 
 	echo $query . "<br>\n"; 
@@ -96,6 +79,7 @@
 	} else {
 	    die("Error : " . $conn->error);
 	}
+
 
 	$conn->close();
 
