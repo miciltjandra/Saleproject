1995488a6604f2306c07a74320d982afa2cbec04
joshuakosasih <joshua.kosasih@gmail.com>
2016-10-06 23:53:28 +0700

    change image attribute datatype

--- saleproject.sql
+++ saleproject.sql
@@ -55,12 +55,12 @@ CREATE TABLE `product` (
   `likes` int(10) DEFAULT '0',
   `added_date` datetime DEFAULT NULL,
   `price` decimal(15,0) NOT NULL,
-  `image` longblob,
+  `image` varchar(300) DEFAULT NULL,
   `seller_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`product_id`),
   KEY `seller` (`seller_id`),
   CONSTRAINT `seller` FOREIGN KEY (`seller_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'sabun','nyabun',3,'2016-03-03 10:00:00',20000,'?',9),(2,'minyak','oil',6,'2016-03-13 15:00:00',30000,'?',9),(3,'kucing','ngeong',5,'2016-03-04 11:00:00',50000,'?',10),(4,'haha','hihihi',NULL,'2016-10-05 05:09:00',12345,'C:UsersJoshua A KosasihDocuments[kuliah] WBDChicks-In-Hats1.jpg',2),(5,'waw','wiw',2,'2016-10-05 18:02:00',10000,'',3);
+INSERT INTO `product` VALUES (1,'sabun','nyabun nyabunan sini hayu',3,'2016-10-06 17:19:00',20000,'',9),(2,'minyak','oil',6,'2016-03-13 15:00:00',30000,'?',9),(3,'kucing','ngeong',5,'2016-03-04 11:00:00',50000,'?',10),(4,'haha','hihihi',NULL,'2016-10-05 05:09:00',12345,'C:UsersJoshua A KosasihDocuments[kuliah] WBDChicks-In-Hats1.jpg',2),(5,'waw','wiw',2,'2016-10-05 18:02:00',10000,'',3),(6,'adi','jadi adi suka sama dia',0,'2016-10-06 17:18:00',500,'',9),(7,'hah','hoh',0,'2016-10-06 17:07:00',1,'',3),(8,'qwer','qwerty',0,'2016-10-06 17:51:00',1234,NULL,9),(10,'wahh','wahaha',0,'2016-10-06 18:37:00',12,'/xampp/htdocs/TugasBesar1_SaleProject/images/Chicks-In-Hats1.jpg',9),(11,'yeuh','yeuhyeuh',0,'2016-10-06 18:44:00',1010,'/xampp/htdocs/TugasBesar1_SaleProject/images/Graphviz.png',9),(12,'asdf',';lkj',0,'2016-10-06 18:46:00',12,'/images/Screenshot (1).png',9),(13,'poiu',';lkj',0,'2016-10-06 18:47:00',1,'images/Screenshot (1).png',9);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,4 +150,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-06 18:43:51
+-- Dump completed on 2016-10-06 23:52:45
