420d497be5fd2a0b736ef53ab2f215742d4cd161
joshuakosasih <joshua.kosasih@gmail.com>
2016-10-07 15:49:24 +0700

    add deletion confirmation

--- yourproducts.php
+++ yourproducts.php
@@ -3,6 +3,13 @@
 	<head>
 		<title> Your Products </title>
 		<link rel="stylesheet" href="style.css">
+		<script>
+			function deleteP(namaP, u_id, p_id) {
+			    if (confirm("Delete product \"" + namaP + "\" from SaleProject?") == true) {
+			        window.location = "actionyproducts.php?id_active=" + u_id + "&product=" + p_id;
+			    }
+			}
+		</script>
 	</head>
 	<?php
 	    require 'header.php';
@@ -26,8 +33,8 @@
 						echo $product["added_date"] . "<br />\n";
 						echo '<div class="product_div" style="">' . "\n";
 							echo "<hr />\n";
-							echo '<div style="float:left">';
-							echo '<img src="' . $product["image"] . '"height=100 width=100/><br />' . "\n";
+							echo '<div style="float:left; width:180px">';
+							echo '<img src="' . $product["image"] . '"height=150 style="max-width:180px"/><br />' . "\n";
 							echo '</div><div style="float:left; margin: 10px 10px 10px 10px">';
 							echo "<strong>\n";
 							echo $product["product_name"]  . "<br />\n";
@@ -39,7 +46,7 @@
 							echo $product["purchases"] . " purchases"  . "<br /><br />\n";
 							echo "<strong>\n";
 							echo '<a href="editproduct.php?id_active=' . $_GET["id_active"] . '&product=' . $product["product_id"] . '" style="color:orange">EDIT</a>'  . "\n";
-							echo '<a href="actionyproducts.php?id_active=' . $_GET["id_active"] . '&product=' . $product["product_id"] . '" style="color:red">DELETE</a>'  . "<br />\n";
+							echo '<a onclick="deleteP(\'' . $product["product_name"] . '\', ' . $_GET["id_active"] . ', ' . $product["product_id"] . ')" style="color:red">DELETE</a>'  . "<br />\n";
 							echo "</strong>\n";
 							echo "</div>";
 						echo "</div>\n";
