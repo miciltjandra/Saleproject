3879b2d80a249a4187f7f214d31e90616be219dc
joshuakosasih <joshua.kosasih@gmail.com>
2016-10-07 10:43:33 +0700

    enable delete_function including uploaded image

--- actionyproducts.php
+++ actionyproducts.php
@@ -27,4 +27,47 @@
 
 		return $result;
 	}
+
+	if (isset($_GET["product"])) {
+
+		$servername = "localhost";
+		$db_username = "wbd";
+		$db_password = "twinbaldchicken";
+		$db_database = "saleproject";
+
+		// Create connection
+		$conn = new mysqli($servername, $db_username, $db_password, $db_database);
+
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+
+		// Get image name for deletion
+		$query = 'SELECT image
+		FROM product
+		WHERE product_id = ' . $_GET["product"];
+
+		$result = $conn->query($query);
+
+		$row = $result->fetch_assoc();
+
+		// Remove data from database
+		$query = 'DELETE FROM product
+		WHERE product_id = ' . $_GET["product"];
+
+		if ($conn->query($query) === TRUE) {
+		    echo "Delete product success<br>\n";
+		    
+		    // Delete uploaded image
+		    unlink($row['image']);
+
+		    header("Location: yourproducts.php?id_active=" . $_GET["id_active"]);
+		} else {
+		    die("Error : " . $conn->error);
+		}
+
+		$conn->close();
+
+	}
 ?>
