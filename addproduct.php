6d8a6f2a323f808089b3da005e1e1b8cb9bca15d
joshuakosasih <joshua.kosasih@gmail.com>
2016-10-07 23:02:32 +0700

    add style and new style class

--- addproduct.php
+++ addproduct.php
@@ -13,25 +13,25 @@
     ?>
 	<body class="middle">
 		
-		<div class=""> Please add your product here </div>
+		<div class="large text"> Please add your product here </div>
 		<br /><hr />
-		<form action="actionadd.php" method="post" id="addform" enctype="multipart/form-data">
+		<form class="text" action="actionadd.php" method="post" id="addform" enctype="multipart/form-data">
 			<input type = "hidden" name = "user" value = <?php echo $_GET["id_active"]?> />
 			Name <br />
-			<input id="add_name" type="text" name="name" oninput="valNotEmpty(this.value, 'add_name')" required /> 
+			<input id="add_name" class="reg_text" type="text" name="name" oninput="valNotEmpty(this.value, 'add_name')" required /> 
 			<br />
 			Description (max 200 chars) <br />
-			<textarea id="add_desc" rows="4" form="addform" name="desc" oninput="valNotEmpty(this.value, 'add_desc')" required></textarea> 
+			<textarea id="add_desc" class="reg_text" rows="4" form="addform" name="desc" oninput="valNotEmpty(this.value, 'add_desc')" required></textarea> 
 			<br />
 			Price (IDR) <br />
-			<input id="add_price" type="number" name="price" oninput="valNumber(this.value, 'add_price')" required /> 
+			<input id="add_price" class="reg_text" type="number" name="price" oninput="valNumber(this.value, 'add_price')" required /> 
 			<br />
 			Photo <br />
 			<input type="file" name="imagefile" accept="image/*" required /> 
-			<p id="">No file chosen</p> 
 			<br />
-			<input type="submit" value="Add" name="addbtn" />
-			<input type="button" value="Cancel" />
+			<input class="submit" type="button" value="Cancel" />
+			<input class="submit" type="submit" value="Add" name="addbtn" />
 		</form>
+		<br class="breaker"/>
 	</body>
 </html>
--- editproduct.php
+++ editproduct.php
@@ -19,25 +19,25 @@
 		?>
 		<div class="text large"> Please update your product here </div>
 		<br /><hr />
-		<form action="actionedit.php" method="post" id="addform" enctype="multipart/form-data">
+		<form class="text" action="actionedit.php" method="post" id="addform" enctype="multipart/form-data">
 			<input type = "hidden" name = "user" value = <?php echo $_GET["id_active"]?> />
 			<input type = "hidden" name = "product" value = <?php echo $_GET["product"]?> />
 			Name <br />
-			<input id="edit_name" type="text" name="name" value = <?php echo $product["product_name"] ?> oninput="valNotEmpty(this.value, 'edit_name')" required/> 
+			<input id="edit_name" class="reg_text" type="text" name="name" value = <?php echo $product["product_name"] ?> oninput="valNotEmpty(this.value, 'edit_name')" required/> 
 			<br />
 			Description (max 200 chars) <br />
-			<textarea id="edit_desc" rows="4" form="addform" name="desc" oninput="valNotEmpty(this.value, 'edit_desc')" required><?php echo $product["description"] ?></textarea> 
+			<textarea id="edit_desc" class="reg_text" rows="4" form="addform" name="desc" oninput="valNotEmpty(this.value, 'edit_desc')" required><?php echo $product["description"] ?></textarea> 
 			<br />
 			Price (IDR) <br />
-			<input id="edit_price" type="number" name="price" value = <?php echo $product["price"] ?> oninput="valNumber(this.value, 'edit_price')" required/> 
+			<input id="edit_price" class="reg_text" type="number" name="price" value = <?php echo $product["price"] ?> oninput="valNumber(this.value, 'edit_price')" required/> 
 			<br />
 			Photo <br />
 			<input type="file" name="imagefile" accept="image/*" value = <?php echo $product["image"] ?> /> 
-			<p id="" ><?php echo $product["image"] ?></p> 
+			<p id="" ><?php echo $product["image"] ?></p>
 			<br />
-			<input type="submit" value="Update" name="updatebtn" />
-			<input type="submit" value="Cancel" name="cancelbtn"/>
+			<input class="submit" type="submit" value="Cancel" name="cancelbtn"/>
+			<input class="submit" type="submit" value="Update" name="updatebtn" />
 		</form>
-
+		<br class="breaker" />
 	</body>
 </html>
